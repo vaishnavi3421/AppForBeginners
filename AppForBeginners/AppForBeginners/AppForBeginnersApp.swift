63efc600058a0e70b2fc316a860fe1cc6572234c
vaishnavi <vasanjay@deloitte.com>
2024-03-01 16:08:55 +0530

    basics

---
+++ AppForBeginners/AppForBeginners/AppForBeginnersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppForBeginnersApp.swift
+//  AppForBeginners
+//
+//  Created by Sanjay, Vaishnavi on 27/02/24.
+//
+
+import SwiftUI
+
+@main
+struct AppForBeginnersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppForBeginners/AppForBeginners/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  AppForBeginners
+//
+//  Created by Sanjay, Vaishnavi on 27/02/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Vaishnavi").padding()
+            .background(Color(.blue))
+        
+        
+        
+    }
+}
+
+#Preview {
+    ContentView()
+}
